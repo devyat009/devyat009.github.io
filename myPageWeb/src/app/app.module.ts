343785c59db4e618e93f5400c4aa8a2315c53f0b
Higor S. <higormotapipoca@gmail.com>
2024-11-28 11:28:40 -0300

    footer and header added

--- myPageWeb/src/app/app.module.ts
+++ myPageWeb/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageFooterComponent } from './pages/shared/page-footer/page-footer.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { PageHeaderComponent } from './pages/shared/page-header/page-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageFooterComponent
+    PageHeaderComponent,
+    PageFooterComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
