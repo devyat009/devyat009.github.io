526a1cd7f70091dbd45c41feb9881a5bd6507509
Higor S. <higormotapipoca@gmail.com>
2024-12-02 11:20:26 -0300

    Fixes, improved header and footer

--- myPageWeb/src/app/pages/shared/page-header/page-header.component.ts
+++ myPageWeb/src/app/pages/shared/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-header.component.css']
 })
 export class PageHeaderComponent implements OnInit {
+  selectedIcon: HTMLElement | null = null;
 
-  constructor() { }
+  constructor(
+    private elRef: ElementRef
+    
+  ) {
+
+  }
 
   ngOnInit() {
   }
 
+
+  ngAfterViewInit(): void {
+    const icons = this.elRef.nativeElement.querySelectorAll('.svg-cursor') as NodeListOf<HTMLElement>;
+
+    icons.forEach((icon) => {
+      icon.addEventListener('click', (event: Event) => {
+        const target = event.target as HTMLElement;
+
+        if (this.selectedIcon === target) {
+          this.selectedIcon.classList.remove('selected');
+          this.selectedIcon = null;
+        } else {
+          if (this.selectedIcon) {
+            this.selectedIcon.classList.remove('selected');
+          }
+          target.classList.add('selected');
+          this.selectedIcon = target;
+          console.log(this.selectedIcon);
+        }
+      });
+    });
+  }
 }
