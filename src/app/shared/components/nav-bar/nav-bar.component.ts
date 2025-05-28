e5d9c84c038235f9877c82551766a440223a4f2b
Higor S. <higor.pereira@websis.com.br>
2025-05-28 14:54:55 -0300

    ajuste para o tailwind funcionar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -2,18 +2,28 @@ import { Component, Renderer2 } from '@angular/core';
 import { Button } from 'primeng/button';
 import { ThemeService } from '../../services/theming.service';
 import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'nav-bar-component',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
   imports: [
     Button,
-    CommonModule
+    CommonModule,
+    MenubarModule,
 
   ],
 })
 export class NavBarComponent {
   theme: boolean = false; // false = light, true = dark
+
+  items: MenuItem[] = [
+    { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
+    { label: 'Sobre', icon: 'pi pi-user', routerLink: '/about' },
+    { label: 'Contato', icon: 'pi pi-envelope', routerLink: '/contact' }
+  ];
+  
   constructor(
     private _themeService: ThemeService
   ) {}
